748704307f1db5809b94f38812c7e3f4865e0692
tjarkrasche <hallo@t-jark.de>
2014-07-15 16:59:07 +0200

    Functions
    Develope some compiler functions

---
+++ compilerfunction.py
@@ -0,0 +1,116 @@
+def compiler(str2betranslated, language):
+    if (language == "morseCode"):
+        strword_space = '/ '
+        Aa = '.- '
+        Bb = '-... '
+        Cc = '-.-. '
+        Dd = '-.. '
+        Ee = '. '
+        Ff = '..-. '
+        Gg = '--. '
+        Hh = '.... '
+        Ii = '.. '
+        Jj = '.--- '
+        Kk = '-.- '
+        Ll = '.-.. '
+        Mm = '-- '
+        Nn = '-. '
+        Oo = '--- '
+        Pp = '.--. '
+        Qq = '--.- '
+        Rr = '.-. '
+        Ss = '... '
+        Tt = '- '
+        Uu = '..- '
+        Vv = '...- '
+        Ww = '.-- '
+        Xx = '-..- '
+        Yy = '-.-- '
+        Zz = '--.. '
+        str2betranslated 
+        global str2morse_output
+        str2betranslated = [s.replace(' ', strword_space)for s in str2betranslated]
+    
+        str2betranslated = [s.replace('A', Aa)for s in str2betranslated]
+        str2betranslated = [s.replace('a', Aa)for s in str2betranslated]
+    
+        str2betranslated = [s.replace('B', Bb)for s in str2betranslated]
+        str2betranslated = [s.replace('b', Bb)for s in str2betranslated]
+    
+        str2betranslated = [s.replace('C', Cc)for s in str2betranslated]
+        str2betranslated = [s.replace('c', Cc)for s in str2betranslated]
+    
+        str2betranslated = [s.replace('D', Dd)for s in str2betranslated]
+        str2betranslated = [s.replace('d', Dd)for s in str2betranslated]
+    
+        str2betranslated = [s.replace('E', Ee)for s in str2betranslated]
+        str2betranslated = [s.replace('e', Ee)for s in str2betranslated]
+    
+        str2betranslated = [s.replace('F', Ff)for s in str2betranslated]
+        str2betranslated = [s.replace('f', Gg)for s in str2betranslated]
+    
+        str2betranslated = [s.replace('G', Gg)for s in str2betranslated]
+        str2betranslated = [s.replace('g', Gg)for s in str2betranslated]
+        
+        str2betranslated = [s.replace('H', Hh)for s in str2betranslated]
+        str2betranslated = [s.replace('h', Hh)for s in str2betranslated]
+        
+        str2betranslated = [s.replace('I', Ii)for s in str2betranslated]
+        str2betranslated = [s.replace('i', Ii)for s in str2betranslated]
+        
+        str2betranslated = [s.replace('J', Jj)for s in str2betranslated]
+        str2betranslated = [s.replace('j', Jj)for s in str2betranslated]
+        
+        str2betranslated = [s.replace('K', Kk)for s in str2betranslated]
+        str2betranslated = [s.replace('k', Kk)for s in str2betranslated]
+        
+        str2betranslated = [s.replace('L', Ll)for s in str2betranslated]
+        str2betranslated = [s.replace('l', Ll)for s in str2betranslated]
+        
+        str2betranslated = [s.replace('M', Mm)for s in str2betranslated]
+        str2betranslated = [s.replace('m', Mm)for s in str2betranslated]
+        
+        str2betranslated = [s.replace('N', Nn)for s in str2betranslated]
+        str2betranslated = [s.replace('n', Nn)for s in str2betranslated]
+        
+        str2betranslated = [s.replace('O', Oo)for s in str2betranslated]
+        str2betranslated = [s.replace('o', Oo)for s in str2betranslated]
+        
+        str2betranslated = [s.replace('P', Pp)for s in str2betranslated]
+        str2betranslated = [s.replace('p', Pp)for s in str2betranslated]
+        
+        str2betranslated = [s.replace('Q', Qq)for s in str2betranslated]
+        str2betranslated = [s.replace('q', Qq)for s in str2betranslated]
+        
+        str2betranslated = [s.replace('R', Rr)for s in str2betranslated]
+        str2betranslated = [s.replace('r', Rr)for s in str2betranslated]
+        
+        str2betranslated = [s.replace('S', Ss)for s in str2betranslated]
+        str2betranslated = [s.replace('s', Ss)for s in str2betranslated]
+        
+        str2betranslated = [s.replace('T', Tt)for s in str2betranslated]
+        str2betranslated = [s.replace('t', Tt)for s in str2betranslated]
+        
+        str2betranslated = [s.replace('U', Uu)for s in str2betranslated]
+        str2betranslated = [s.replace('u', Uu)for s in str2betranslated]
+        
+        str2betranslated = [s.replace('V', Vv)for s in str2betranslated]
+        str2betranslated = [s.replace('v', Vv)for s in str2betranslated]
+        
+        str2betranslated = [s.replace('W', Ww)for s in str2betranslated]
+        str2betranslated = [s.replace('w', Ww)for s in str2betranslated]
+        
+        str2betranslated = [s.replace('X', Xx)for s in str2betranslated]
+        str2betranslated = [s.replace('x', Xx)for s in str2betranslated]
+        
+        str2betranslated = [s.replace('Y', Yy)for s in str2betranslated]
+        str2betranslated = [s.replace('y', Yy)for s in str2betranslated]
+        
+        str2betranslated = [s.replace('Z', Zz)for s in str2betranslated]
+        str2betranslated = [s.replace('z', Zz)for s in str2betranslated]
+        
+        str2morse_output = ''.join(str2betranslated)
+        return
+compiler("jojojo", "morseCode")
+print str2morse_output 
+print "Sao zack es wead losgehen! \n Ih wead hoffentlich die HAusssaufgaben gemacht ham   ne \ncassy wead auch schon dabei sein fedammt nochmaaa"
