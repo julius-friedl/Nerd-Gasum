eed69590efc4aef2c99a18b248a7514da1457837
julius-friedl <julius5249@hotmail.com>
2014-07-15 17:28:19 +0200

    Function implementation --open

--- main.py
+++ main.py
@@ -32,16 +32,18 @@ class MainFrame(wx.Frame):
 ##########Drop-Down########################################################################//-->
 		hbox1 = wx.BoxSizer(wx.HORIZONTAL)
 		parse_options = ['Morse-Code','Binary-Code (n/a)', 'Leetspeak (n/a)']
-		dropdown = wx.ComboBox(mainpanel, -1,'-None-', size=(175, 25), choices=parse_options, style=wx.CB_READONLY)
+		dropdown = wx.ComboBox(mainpanel, -1,'-None-', size=(175, 25), choices=parse_options, style=wx.CB_READONLY|wx.TE_PROCESS_ENTER)
 		hbox1.Add(dropdown, flag=wx.RIGHT, border=8)
 		self.Bind(wx.EVT_COMBOBOX, self.onComboSelection, dropdown)
+		self.Bind(wx.EVT_TEXT_ENTER, self.onCompile, dropdown)
 		
 		vbox.Add(hbox1, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
 ##########Input-Text############################################################################//-->		
 		hbox2 = wx.BoxSizer(wx.HORIZONTAL)
-		input_text =  wx.TextCtrl(mainpanel, style=wx.TE_MULTILINE)
+		input_text =  wx.TextCtrl(mainpanel, style=wx.TE_MULTILINE|wx.TE_PROCESS_ENTER)
 		hbox2.Add(input_text, proportion=1, flag=wx.EXPAND)
        		vbox.Add(hbox2, proportion=1, flag=wx.LEFT|wx.RIGHT|wx.TOP|wx.EXPAND, border=10)
+       		self.Bind(wx.EVT_TEXT_ENTER, self.onCompile, input_text)
        		
        		vbox.Add((-1, 35))
 ##########Output-Text############################################################################//-->		 
@@ -58,8 +60,7 @@ class MainFrame(wx.Frame):
 		Image_make = wx.Image(ImageFile, wx.BITMAP_TYPE_ANY).ConvertToBitmap()
 		Image_btn = wx.BitmapButton(mainpanel, id=-1, bitmap=Image_make, pos=(0,0), size = ((Image_make.GetWidth()+7), (Image_make.GetHeight()+7)), style = wx.NO_BORDER)
 		self.Bind(wx.EVT_BUTTON, self.onCompile, Image_btn)
-		
-		
+				
 	def onSize(self, event):
 		event.Skip()
 		mainpanel.SetSize(self.GetClientSizeTuple())
@@ -76,45 +77,48 @@ class MainFrame(wx.Frame):
 		postuple = (x, y)
 		Image_btn.SetPosition( postuple )
 		
-		
-	
 	def onCompile(self, event):
 		in_str = input_text.GetValue()
 		print in_str
 		current_select = dropdown.GetValue()
 		
-		if current_select == 'Morse-Code':
-			#parseMorseCode()
-			output_text.SetForegroundColour('black')
-			out_str = in_str
-			output_text.SetValue( out_str )
-			pass
-			
-		elif current_select == 'Binary-Code (n/a)':
-			#parseBinaryCode()	
-			output_text.SetForegroundColour('black')
-			out_str = in_str
-			output_text.SetValue( out_str )
-			pass
-			
-		elif current_select == 'Leetspeak (n/a)':
-			#parseLeetSpeak()
-			output_text.SetForegroundColour('black')	
-			out_str = in_str
-			output_text.SetValue( out_str )
-			pass
+		compiler(in_str, current_select)
 		
-		else:
-			output_text.SetForegroundColour('Red')
-			out_str = '!-- Please select a Code to Compile to  --!'
-			output_text.SetValue( out_str )	
+#		if current_select == 'Morse-Code':
+#			test()
+#			#parseMorseCode()
+#			output_text.SetForegroundColour('black')
+#			out_str = in_str
+#			output_text.SetValue( out_str )
+#			pass
+#			
+#		elif current_select == 'Binary-Code (n/a)':
+#			#parseBinaryCode()	
+#			output_text.SetForegroundColour('black')
+#			out_str = in_str
+#			output_text.SetValue( out_str )
+#			pass
+#			
+#		elif current_select == 'Leetspeak (n/a)':
+#			#parseLeetSpeak()
+#			output_text.SetForegroundColour('black')	
+#			out_str = in_str
+#			output_text.SetValue( out_str )
+#			pass
+#		
+#		else:
+#			output_text.SetForegroundColour('Red')
+#			out_str = '!-- Please select a Code to Compile to  --!'
+#			output_text.SetValue( out_str )	
 			
 	
 	def onComboSelection(self, event):
 		current_select = dropdown.GetValue()
 		print current_select		
-		
-		
+			
+
+def test():
+	print 'it works'
 		
 #run-it
 if __name__ == '__main__':
